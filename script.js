0fa5cdaef0df71f90a09d1604b3e093a0983aabc
Tech-A19 <Tech-A19@Tech-A19s-MacBook-Air.local>
2017-11-02 22:35:22 +0000

    done the variables and first function

---
+++ script.js
@@ -0,0 +1,69 @@
+//start
+$(function (event) {
+  console.log('DOM is ready');
+
+
+
+//start of the gamw
+$ (function playGame() {
+
+  var player1 = play1();
+  var player2 = play2();
+  var win = winner();
+
+   
+
+
+
+//players turn
+
+
+
+
+playgame();
+
+});
+
+// var array = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[0,3,6],[6,4,2],[1,4,7],[2,5,8]];
+// if (array === true) {
+// $.each(array, function() {
+//   console.log("win");
+//   // else {
+//   //   console.log("lose");
+//   // }
+// })
+// }
+
+// $.each(yourObject, function( index, value ) {
+//     console.log(value.values);
+//     console.log(value.input);
+// });
+
+// function winCase() {
+//   switch (win) {
+//     case expression:
+//
+//       break;
+//     default:
+//
+//   }
+// }
+//
+//
+// winCase();
+
+
+//create player 1 and player 2
+//reset button not working.
+
+ // $("#reset").css("background-color", "red");
+
+ // $('#reset').click(function(){
+  //            $('td')[0].reset();
+  // });
+
+// $("td").append("<td>X</td>");
+
+// $('button').one('click', function() {
+//   $('#td').append($("<td>X</td>");
+// });
